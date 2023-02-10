23fb71d5587f45519a2a0d292587adb14b14acd0
lairacouto <contato.lairacouto@gmail.com>
2023-02-10 14:38:50 -0300

    Estudos JS

---
+++ scripts.js
@@ -0,0 +1,18 @@
+let weight 
+
+console.log(typeof weight)
+
+let name = "Mayk"
+let age = 23
+let stars = 4.8
+let isSubscribed = true
+
+let student = {
+    name: "Mayk",
+    age: 23,
+    stars: 4.8,
+    weight: 74.8,
+    isSubscribed: true,
+}
+
+console.log(`${student.name} de idade ${student.age} pesa ${student.weight} kg.`)
